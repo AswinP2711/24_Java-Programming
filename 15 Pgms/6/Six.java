4106e897bc61cb5132ab76a618e9c148247cb837
AswinP2711 <aswinp2711@gmail.com>
2021-05-27 12:11:56 +0530

    13 Pgms

---
+++ 15 Pgms/6/Six.java
@@ -0,0 +1,16 @@
+public class Six {
+    public static void main(String[] args) {
+        int a=6;
+        int b=12;
+        int c=8;
+        if (a>b) {
+            if(a>c){
+                System.out.println(a+" is greatest number");
+            }else{
+                System.out.println(c+" is greatest number");
+            }
+        }else{
+            System.out.println(b+" is greatest number");
+        }
+    }
+}
