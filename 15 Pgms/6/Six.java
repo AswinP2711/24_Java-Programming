a95a12931ecc8871f3c7cf1c1ac3915cb9efdc95
AswinP2711 <aswinp2711@gmail.com>
2021-06-03 16:45:53 +0530

    6th Program Correction

--- 15 Pgms/6/Six.java
+++ 15 Pgms/6/Six.java
@@ -2,7 +2,7 @@ public class Six {
     public static void main(String[] args) {
         int a=6;
         int b=12;
-        int c=8;
+        int c=15;
         if (a>b) {
             if(a>c){
                 System.out.println(a+" is greatest number");
@@ -10,7 +10,12 @@ public class Six {
                 System.out.println(c+" is greatest number");
             }
         }else{
-            System.out.println(b+" is greatest number");
+            if (b>c) {
+                System.out.println(b+" is greatest number");
+            }else{
+                System.out.println(c+" is greatest number");
+            }
+            
         }
     }
 }
