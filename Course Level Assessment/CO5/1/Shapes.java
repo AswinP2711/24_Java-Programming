2a9ed0566265ad5f65bb62d61ae749a37a7dc721
AswinP2711 <aswinp2711@gmail.com>
2021-09-23 19:14:40 +0530

    CO5 1&2

--- Course Level Assessment/CO5/1/Shapes.java
+++ Course Level Assessment/CO5/1/Shapes.java
@@ -4,10 +4,10 @@ import java.applet.*;
 <applet code="Shapes" width=800 height=800>
 </applet>
 */
-public class Shapes extends Applet{
-public void paint(Graphics g){
-g.drawLine(10,10,100,100);
-g.drawRect(100,150,100,150);
-g.drawOval(300,300,300,300);
-}
+public class Shapes extends Applet {
+  public void paint(Graphics g) {
+    g.drawLine(10, 10, 100, 100);
+    g.drawRect(100, 150, 100, 150);
+    g.drawOval(300, 300, 300, 300);
+  }
 }
--- Course Level Assessment/CO5/2/Maximum3.java
+++ Course Level Assessment/CO5/2/Maximum3.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.applet.*;
 /*
@@ -8,52 +7,45 @@ import java.applet.*;
 <param name="c" value="30">
 </applet>
 */
-public class Maximum3 extends Applet
-{
-	int a;
-	int b;
-	int c;
-	int d;
-	String str;
+public class Maximum3 extends Applet {
+  int a;
+  int b;
+  int c;
+  int d;
+  String str;
 
-	public void start()
-	{
-		String s1;
+  public void start() {
+    String s1;
 
-		s1 = getParameter("a");
-		a = Integer.parseInt(s1);
+    s1 = getParameter("a");
+    a = Integer.parseInt(s1);
 
-		s1 = getParameter("b");
-		b = Integer.parseInt(s1);
+    s1 = getParameter("b");
+    b = Integer.parseInt(s1);
 
-		s1 = getParameter("c");
-		c= Integer.parseInt(s1);
-	}
+    s1 = getParameter("c");
+    c = Integer.parseInt(s1);
+  }
 
-	public void paint(Graphics g)
-	{
-if (a>b) {
-            if(a>c){
-                d=a;
-            }else{
-                d=c;
-            }
-        }else{
-            if (b>c) {
-                d=b;
-            }else{
-                d=c;
-            }
-            
-        }
-		
+  public void paint(Graphics g) {
+    if (a > b) {
+      if (a > c) {
+        d = a;
+      } else {
+        d = c;
+      }
+    } else {
+      if (b > c) {
+        d = b;
+      } else {
+        d = c;
+      }
 
+    }
 
-
-
-		g.drawString("a is   " + a, 10, 50);
-		g.drawString("b is   " + b, 10, 100);
-		g.drawString("c is   " + c, 10, 150);
-		g.drawString("max of three numbers is " + d, 10, 200);
-	}
+    g.drawString("a is   " + a, 10, 50);
+    g.drawString("b is   " + b, 10, 100);
+    g.drawString("c is   " + c, 10, 150);
+    g.drawString("max of three numbers is " + d, 10, 200);
+  }
 }
