d0453f82b3b3e0dbf1ecf2d0f9ad01ec9848b1ab
AswinP2711 <aswinp2711@gmail.com>
2021-09-26 16:46:32 +0530

    CO5 & 6

--- Course Level Assessment/CO6/6/MyServer.java
+++ Course Level Assessment/CO6/6/MyServer.java
@@ -1,44 +1,37 @@
 import java.io.*;
 import java.net.*;
-  
-public class MyServer
-{
-    public static void main(String[] args) throws IOException
-    {
-        DatagramSocket ds = new DatagramSocket(1234);
-        byte[] receive = new byte[65535];
-  
-        DatagramPacket DpReceive = null;
-        while (true)
-        {
-            DpReceive = new DatagramPacket(receive, receive.length);
 
-            ds.receive(DpReceive);
-  
-            System.out.println("Client:-" + data(receive));
+public class MyServer {
+  public static void main(String[] args) throws IOException {
+    DatagramSocket ds = new DatagramSocket(1234);
+    byte[] receive = new byte[65535];
 
-            if (data(receive).toString().equals("bye"))
-            {
-                System.out.println("Client sent bye.....EXITING");
-                break;
-            }
-  
-            receive = new byte[65535];
-        }
+    DatagramPacket DpReceive = null;
+    while (true) {
+      DpReceive = new DatagramPacket(receive, receive.length);
+
+      ds.receive(DpReceive);
+
+      System.out.println("Client:-" + data(receive));
+
+      if (data(receive).toString().equals("bye")) {
+        System.out.println("Client sent bye.....EXITING");
+        break;
+      }
+
+      receive = new byte[65535];
     }
-  
+  }
 
-    public static StringBuilder data(byte[] a)
-    {
-        if (a == null)
-            return null;
-        StringBuilder ret = new StringBuilder();
-        int i = 0;
-        while (a[i] != 0)
-        {
-            ret.append((char) a[i]);
-            i++;
-        }
-        return ret;
+  public static StringBuilder data(byte[] a) {
+    if (a == null)
+      return null;
+    StringBuilder ret = new StringBuilder();
+    int i = 0;
+    while (a[i] != 0) {
+      ret.append((char) a[i]);
+      i++;
     }
+    return ret;
+  }
 }
